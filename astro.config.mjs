631543f5f486ac4dfe68ef548baa06bb6f995326
mauricionlz2018-tech <mauricionlz2018@gmail.com>
2025-11-17 20:04:41 -0600

    Nuevo04

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://mauricionlz2018-tech.github.io/',
   base: '/mauricionlz2018.github.io/',
-  output: 'static',
   vite: {
     plugins: [tailwindcss()]
   }
